43fe7883f02e9a58742a6ccec96f8ef15a207d97
tolaseadegbite <tolasekelvin@gmail.com>
2024-10-18 14:02:31 +0200

    Add views/urls for posts

--- wey_backend/post/serializers.py
+++ wey_backend/post/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'body', 'created_by',)
+        fields = ('id', 'body', 'created_by', 'created_at',)
