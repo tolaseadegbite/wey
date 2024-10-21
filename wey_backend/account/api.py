fadc344887c107129f1246efd9b1c82bb1aa5910
tolaseadegbite <tolasekelvin@gmail.com>
2024-10-21 21:38:31 +0200

    Handle friend requests

--- wey_backend/account/api.py
+++ wey_backend/account/api.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from .forms import SignupForm
 from .models import User, FriendshipRequest
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, FriendshipRequestSerializer
 
 @api_view(['GET'])
 def me(request):
@@ -41,6 +41,42 @@ def signup(request):
 def send_friendship_request(request, pk):
     user = User.objects.get(pk=pk)
 
-    friendship_request = FriendshipRequest(created_for=user, created_by=request.user)
+    friendship_request = FriendshipRequest.objects.create(created_for=user, created_by=request.user)
 
     return JsonResponse({'message': 'friendship request created'})
+
+def friends(request, pk):
+    user = User.objects.get(pk=pk)
+    requests = []
+
+    if user == request.user:
+        requests = FriendshipRequest.objects.filter(created_for=request.user, status=FriendshipRequest.SENT)
+        requests = FriendshipRequestSerializer(requests, many=True)
+        requests = requests.data
+
+    friends = user.friends.all()
+
+    return JsonResponse({
+        'user': UserSerializer(user).data,
+        'friends': UserSerializer(friends, many=True).data,
+        'requests': requests
+    }, safe=False)
+
+@api_view(['POST'])
+def handle_request(request, pk, status):
+    user = User.objects.get(pk=pk)
+    friendship_request = FriendshipRequest.objects.filter(created_for=request.user).get(created_by=user)
+    friendship_request.status = status
+    friendship_request.save()
+
+    user.friends.add(request.user)
+    user.friends_count = user.friends_count + 1
+    user.save()
+
+    request_user = request.user
+    request_user.friends_count = request_user.friends_count + 1
+    request_user.save()
+
+    # notification = create_notification(request, 'accepted_friendrequest', friendrequest_id=friendship_request.id)
+
+    return JsonResponse({'message': 'friendship request updated'})
--- wey_backend/account/urls.py
+++ wey_backend/account/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name="token_obtain"),
     path('refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('friends/request/<uuid:pk>/', api.send_friendship_request, name="send_friendship_request"),
+    path('friends/', api.friends, name="friends"),
+    path('friends/<uuid:pk>/<str:status>/', api.handle_request, name='handle_request'),
 ]
